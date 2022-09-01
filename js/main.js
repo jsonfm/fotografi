775296e9d6399ec6131339d10d8ce168722d9026
Jason Francisco Macas Mora <franciscomacas3@gmail.com>
2022-09-01 12:51:50 -0500

    header updated

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 const toggleBtn = document.querySelector('#toggle');
 const toggleIcon = document.querySelector('#toggle-icon');
 const navigation = document.querySelector('#navigation');
+const header = document.querySelector('header');
+
 
 const toggleMenu = () => {
     if (toggleIcon.classList.contains('uil-bars')){
@@ -14,3 +16,12 @@ const toggleMenu = () => {
 }
 
 toggleBtn.addEventListener('click', toggleMenu);
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 100){
+        header.classList.add('scrolled');
+    }else{
+        header.classList.remove('scrolled');
+    }
+    console.log('scroll: ', window.scrollY);
+})
